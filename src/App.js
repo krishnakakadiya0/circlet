2368f6b7800ae8318937d86095de91b903569187
krishnakakadiya0 <krishnakakadiya0000@gmail.com>
2022-05-03 09:49:21 +0530

    medicine data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  let data = [
+    {
+      id: 101,
+      name: "Abacavir",
+      quantity: 25,
+      price: 150,
+      expiry: 2022,
+      status: true,
+    },
+    {
+      id: 102,
+      name: "Eltrombopag",
+      quantity: 90,
+      price: 550,
+      expiry: 2021,
+      status: true,
+    },
+    {
+      id: 103,
+      name: "Meloxicam",
+      quantity: 85,
+      price: 450,
+      expiry: 2025,
+      status: false,
+    },
+    {
+      id: 104,
+      name: "Allopurinol",
+      quantity: 50,
+      price: 600,
+      expiry: 2023,
+      status: true,
+    },
+    {
+      id: 105,
+      name: "Phenytoin",
+      quantity: 63,
+      price: 250,
+      expiry: 2021,
+      status: false,
+    },
+  ];
+  let filterData = data.filter((v, i) => v.expiry >= 2022);
+  let total = filterData.reduce((ass, v, i) => ass + v.price, 0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <table border="1">
+        <tr>
+          <th>Id</th>
+          <th>Name</th>
+          <th>Quantity</th>
+          <th>Expiry</th>
+          <th>Price</th>
+          <th>Total</th>
+        </tr>
+        {filterData.map(({id, name, quantity, expiry, price}, i) => {
+          return (
+            <tr>
+              <td>{id}</td>
+              <td>{name}</td>
+              <td>{quantity}</td>
+              <td>{expiry}</td>
+              <td>{price}</td>
+              {i === 0 ? <td rowspan={filterData.length}>{total}</td> : null}
+            </tr>
+          );
+        })}
+      </table>
+    </>
   );
 }
 
